2da28a3f474485fe164e2bb1fdca8d343229276c
jiyali <762115542@qq.com>
2019-09-23 21:22:34 +0800

    添加新题解

--- 11_01快速排序.py
+++ 11_01快速排序.py
@@ -1,3 +1,4 @@
+# 方法一：先选择第一个元素作为基准值，分为小于基准值的部分和大于基准值的部分，然后对这两部分分别以第一个元素为基准值递归
 def quicksort(array):
     if len(array) < 2:
         return array
@@ -12,3 +13,22 @@ def quicksort(array):
 
 
 print(quicksort([30, 24, 5, 58, 18, 36, 12, 42, 39]))
+
+# 方法二：选择任意一个数作为基准
+from random import *
+
+
+def quicksort1(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = choice(array)
+
+        less = [i for i in array if i < pivot]
+
+        greater = [i for i in array if i > pivot]
+
+        return quicksort1(less) + [pivot] * array.count(pivot) + quicksort1(greater)
+
+
+print(quicksort1([30, 24, 5, 58, 18, 36, 12, 42, 39]))
