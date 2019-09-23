2bfe2a9a914be1bae6b58aada563906c9b5cd4d8
jiyali <762115542@qq.com>
2019-09-23 20:49:08 +0800

    添加新题解

---
+++ 11_01快速排序.py
@@ -0,0 +1,14 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+
+        less = [i for i in array[1:] if i < pivot]
+
+        greater = [i for i in array[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+print(quicksort([30, 24, 5, 58, 18, 36, 12, 42, 39]))
