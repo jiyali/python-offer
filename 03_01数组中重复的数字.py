60aa2ed666b5d1f32fa8a71b3cece54c0efd4e7a
jiyali <762115542@qq.com>
2019-09-24 16:51:17 +0800

    添加新题解

--- 03_01数组中重复的数字.py
+++ 03_01数组中重复的数字.py
@@ -1,26 +1,35 @@
-# 找出数组中重复的数字
+# 题目：在一个长度为n的数组里的所有数字都在0到n-1的范围内。
+#       数组中某些数字是重复的，但不知道有几个数字是重复的。
+#       也不知道每个数字重复几次。请找出数组中任意一个重复的数字。
+#       例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
 # 思路：从下标0开始，对每个元素，若numbers[i]不等于i，则交换numbers[i]和numbers[numbers[i]]，
 #      直至i和numbers[i]相等继续循环，或numbers[i]和numbers[numbers[i]]相等即遇到重复元素返回True
 # 时间复杂度O(n），空间复杂度O(n)
 
 
-def duplicate(numbers, duplication):
-    if numbers is None or len(numbers) == 0:
-        return False
-    for i in numbers:
-        if i < 0 or i >= len(numbers):
+class Solution:
+    # 这里要特别注意~找到任意重复的一个值并赋值到duplication[0]
+    # 函数返回True/False
+    def duplicate(self, numbers, duplication):
+        # write code here
+        if numbers is None:
             return False
+        for i in numbers:
+            if i < 0 or i > len(numbers):
+                return False
+        for i in range(len(numbers)):
+            while i != numbers[i]:
+                if numbers[i] == numbers[numbers[i]]:
+                    duplication[0] = (numbers[i])
+                    return True
+                else:
+                    tmp = numbers[numbers[i]]
+                    numbers[numbers[i]] = numbers[i]
+                    numbers[i] = tmp
+        return False
 
-    for i in range(len(numbers)):
-        while i != numbers[i]:
-            if numbers[i] == numbers[numbers[i]]:
-                duplication.append(numbers[i])
-                break
-            else:
-                tmp = numbers[numbers[i]]
-                numbers[numbers[i]] = numbers[i]
-                numbers[i] = tmp
-    return duplication
 
-
-print(duplicate(numbers=[2, 3, 1, 0, 2, 5, 3, 7, 9, 10, 7], duplication=[]))
+s = Solution()
+test = [2, 3, 1, 0, 2, 5, 3]
+dupulication = [0]
+print(s.duplicate(test, dupulication))
