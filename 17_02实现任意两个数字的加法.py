72db696b893f6d27d1bbb679973644387a1dbc73
jiyali <762115542@qq.com>
2019-09-25 21:58:11 +0800

    添加新题解

--- 17_02实现任意两个数字的加法.py
+++ 17_02实现任意两个数字的加法.py
@@ -1,36 +1,36 @@
 class Solution(object):
-    def big_number_sum(self, s1, s2):
+    def big_number_sum(self, number1, number2):
         """首先写出两个整数相加和相减的函数，然后根据两个数的正负号分四种类别计算"""
-        if s1[0] != '-' and s2[0] != '-':
-            self.big_positive_number_sum(s1, s2)
-        elif s1[0] == '-' and s2[0] != '-':
-            s1 = s1[1:]
-            self.big_positive_number_minus(s2, s1)
-        elif s1[0] != '-' and s2[0] == '-':
-            s2 = s2[1:]
-            self.big_positive_number_minus(s1, s2)
+        if number1[0] != '-' and number2[0] != '-':
+            self.big_positive_number_sum(number1, number2)
+        elif number1[0] == '-' and number2[0] != '-':
+            number1 = number1[1:]
+            self.big_positive_number_minus(number2, number1)
+        elif number1[0] != '-' and number2[0] == '-':
+            number2 = number2[1:]
+            self.big_positive_number_minus(number1, number2)
         else:
-            s1 = s1[1:]
-            s2 = s2[1:]
+            number1 = number1[1:]
+            number2 = number2[1:]
             print('-', end="")
-            self.big_positive_number_sum(s1, s2)
+            self.big_positive_number_sum(number1, number2)
 
-    def big_positive_number_sum(self,s1, s2):
+    def big_positive_number_sum(self,number1, number2):
         """两个大正数相加，首先对齐(补0)，然后按加法计算规则计算"""
         L1 = [0]
         L2 = [0]
-        for i in range(0, len(s1)):
-            L1.append(int(s1[i]))
-        for i in range(0, len(s2)):
-            L2.append(int(s2[i]))
+        for i in range(0, len(number1)):
+            L1.append(int(number1[i]))
+        for i in range(0, len(number2)):
+            L2.append(int(number2[i]))
 
-        if (len(s1) > len(s2)):
-            for i in range(len(s1) - len(s2)):
-                L2.reverse()
+        if len(number1) > len(number2):
+            for i in range(len(number1) - len(number2)):
+                L2.reverse()  # 对列表的元素进行反向排序
                 L2.append(0)
                 L2.reverse()
-        elif (len(s1) <= len(s2)):
-            for i in range(len(s2) - len(s1)):
+        elif len(number1) <= len(number2):
+            for i in range(len(number2) - len(number1)):
                 L1.reverse()
                 L1.append(0)
                 L1.reverse()
@@ -52,22 +52,22 @@ class Solution(object):
                 print(L1[i], end='')
         print()
 
-    def big_positive_number_minus(self,s1, s2):
+    def big_positive_number_minus(self,number1, number2):
         """减法：注意一点，如果s1减去s2得到的第一位的数字是-1，那么说明s2更大，先打印一个负号，然后调用big_positive_number_minus(s2, s1)"""
         L1 = [0]
         L2 = [0]
-        for i in range(0, len(s1)):
-            L1.append(int(s1[i]))
-        for i in range(0, len(s2)):
-            L2.append(int(s2[i]))
+        for i in range(0, len(number1)):
+            L1.append(int(number1[i]))
+        for i in range(0, len(number2)):
+            L2.append(int(number2[i]))
 
-        if (len(s1) > len(s2)):
-            for i in range(len(s1) - len(s2)):
+        if (len(number1) > len(number2)):
+            for i in range(len(number1) - len(number2)):
                 L2.reverse()
                 L2.append(0)
                 L2.reverse()
-        elif (len(s1) <= len(s2)):
-            for i in range(len(s2) - len(s1)):
+        elif (len(number1) <= len(number2)):
+            for i in range(len(number2) - len(number1)):
                 L1.reverse()
                 L1.append(0)
                 L1.reverse()
@@ -88,7 +88,7 @@ class Solution(object):
                     print()
         elif L1[0] != 0:
             print('-', end="")
-            self.big_positive_number_minus(s2, s1)
+            self.big_positive_number_minus(number2, number1)
 
 
 s = Solution()
