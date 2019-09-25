fa0aaa5a304fc8c2fd6575e1c3d2cd5792fba472
jiyali <762115542@qq.com>
2019-09-25 21:03:56 +0800

    添加新题解

--- 17_打印从1到最大的n位数.py
+++ 17_打印从1到最大的n位数.py
@@ -17,6 +17,7 @@ class Solution(object):
 s = Solution()
 s.print1ToMax(3)
 
+
 # 思路二：大数问题：用字符串模拟加法来解决
 #         将字符串初始化全0，每次为字符串表示的数字+1，再打印出来
 
@@ -45,21 +46,21 @@ class Solution1(object):
             nSum = int(number[n]) + isTakeOver
 
             # 实现模拟加法
-            if n == Length - 1:      # 判断当前n是不是在最低位，也就是个位
-                nSum += 1            # 如果在个位，则自增1
+            if n == Length - 1:  # 判断当前n是不是在最低位，也就是个位
+                nSum += 1  # 如果在个位，则自增1
 
-            if nSum == 10:           # 进位
-                if n == 0:           # 判断是否溢出
+            if nSum == 10:  # 进位
+                if n == 0:  # 判断是否溢出
                     isOverFlow = True
                     return isOverFlow
-                else:                # 如果不是溢出，进位
-                    isTakeOver = 1   # 进位标志位置1
+                else:  # 如果不是溢出，进位
+                    isTakeOver = 1  # 进位标志位置1
                     number[n] = '0'  # 将当前位置的数字归‘0’
             else:
                 number[n] = str(nSum)
                 break
 
-            n -= 1                   # n向前移动，观察更高位是否也满足进位
+            n -= 1  # n向前移动，观察更高位是否也满足进位
         return isOverFlow
 
     def PrintNum(self, number):
@@ -71,3 +72,37 @@ class Solution1(object):
 
 s = Solution1()
 s.print1ToMax(3)
+
+
+# 思路三： 转换为数字排列的递归
+
+
+class Solution3(object):
+    def Print1ToMax(self, n):
+        if n <= 0:
+            return
+
+        ListNum = ['0'] * n
+
+        for i in range(10):
+            ListNum[0] = str(i)
+            self.Print1ToMaxCore(ListNum, n, 0)
+
+    def Print1ToMaxCore(self, number, length, index):
+        if index == length - 1:
+            self.PrintNumber(number)
+            return
+        for i in range(10):
+            number[index + 1] = str(i)
+            self.Print1ToMaxCore(number, length, index + 1)
+
+    def PrintNumber(self, number):
+        for i in range(len(number)):
+            if number[i] != '0':
+                print(''.join(number[i:]))  # join() 方法用于将序列中的元素以指定的字符连接生成一个新的字符串。
+                break
+
+
+s = Solution3()
+print('here')
+s.Print1ToMax(3)
