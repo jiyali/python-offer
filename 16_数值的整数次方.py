b767dbdbcdbfc8ffca99fb689a18e9dfe984f238
jiyali <762115542@qq.com>
2019-09-25 15:36:15 +0800

    添加新题解

--- 16_数值的整数次方.py
+++ 16_数值的整数次方.py
@@ -1,6 +1,6 @@
 # 题目：给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
 #      保证base和exponent不同时为0
-
+# 普通做法：
 
 class Solution(object):
     def power(self, base, exponent):
@@ -22,3 +22,24 @@ class Solution(object):
 s = Solution()
 print(s.power(2, -3))
 
+
+# 高效的做法，可以使用递归，同时用右移来代替/2
+class Solution1(object):
+    def power(self, base, exponent):
+
+        if exponent == 0:
+            return 1
+        if exponent == 1:
+            return base
+        if exponent == -1:
+            return 1 / base
+
+        result = self.power(base, exponent >> 1)  # 求余运算
+        result *= result
+        if (exponent & 0x1) == 1:  # 判断奇数还是偶数
+            result *= base
+        return result
+
+
+s1 = Solution1()
+print(s1.power(2, -3))
