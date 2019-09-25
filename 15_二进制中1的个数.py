eb8106124edac835224d7535fda3c3eb9c8c601a
jiyali <762115542@qq.com>
2019-09-25 13:27:36 +0800

    添加新题解

--- 15_二进制中1的个数.py
+++ 15_二进制中1的个数.py
@@ -1,5 +1,6 @@
 # 题目：输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
 # 思路：补码的表示方法是：正数的补码是本身，负数的补码在其原码的基础上a符号位不变，其余各位取反（求反码）最后加一
+#       把一个整数减去1，再和原来整数做与运算，会把该整数最右边的1变成0，那么一个整数有多少个1，就能做多少次这种运算
 
 
 class Solution(object):
@@ -14,6 +15,15 @@ class Solution(object):
             n = (n - 1) & n  # 用于消去最后一位的1
         return count
 
+
+S = Solution()
+print(S.NumberOf1(-1))
+
+
+# 思路二：将输入的整数用二进制表示出来，数1的个数即可
+
+class Solution1(object):
+
     def NumberOf2(self, n):
 
         if n < 0:
@@ -23,9 +33,5 @@ class Solution(object):
         return s.count('1')
 
 
-S = Solution()
-print(S.NumberOf1(-1))
-print(S.NumberOf2(-1))
-
-
-# 思路二：
+s1 = Solution1()
+print(s1.NumberOf2(-1))
