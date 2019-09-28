34711c0323dd6c4e050f4d21da221de1b4653b12
jiyali <762115542@qq.com>
2019-09-28 16:36:56 +0800

    添加新题解

--- 22_02求链表的中间节点.py
+++ 22_02求链表的中间节点.py
@@ -1,4 +1,22 @@
 # 题目：求链表的中间节点，如果总结点数为奇数就是中间那个，如果是偶数就返回中间两个节点的任意一个。
 
 
-class Solution()
+class ListNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class Solution(object):
+    def FindMidNode(self, head):
+        if head is None:
+            return None
+
+        node1 = head
+        node2 = head
+
+        while node1 is not None and node1.next is not None:
+            node2 = node2.next
+            node1 = node1.next.next
+        return node2
+
