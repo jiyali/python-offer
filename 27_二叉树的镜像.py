51e2593d6bed60c581b5ff0d90ebefe1bb41974c
jiyali <762115542@qq.com>
2019-10-08 11:01:51 +0800

    添加新题解

---
+++ 27_二叉树的镜像.py
@@ -0,0 +1,40 @@
+# 题目：操作给定的二叉树，将其变换为源二叉树的镜像。
+# 思路：遍历树，如果根节点存在子节点，则交换左右子节点的值
+
+
+class TreeNode(object):
+    def __init__(self, data):
+        self.val = data
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    # 返回镜像树的根节点
+    def Mirror(self, root):
+        # write code here
+        if root is None:
+            return None
+        root.left, root.right = root.right, root.left
+        self.Mirror(root.left)
+        self.Mirror(root.right)
+
+
+pNode1 = TreeNode(8)
+pNode2 = TreeNode(6)
+pNode3 = TreeNode(10)
+pNode4 = TreeNode(5)
+pNode5 = TreeNode(7)
+pNode6 = TreeNode(9)
+pNode7 = TreeNode(11)
+
+pNode1.left = pNode2
+pNode1.right = pNode3
+pNode2.left = pNode4
+pNode2.right = pNode5
+pNode3.left = pNode6
+pNode3.right = pNode7
+
+S = Solution()
+S.Mirror(pNode1)
+print(pNode1.right.left.val)
