5a1712d2be3fb46dfef6576f87f37a00a5b5ee9f
jiyali <762115542@qq.com>
2019-10-09 09:08:08 +0800

    添加新题解

--- 32_从上到下打印二叉树.py
+++ 32_从上到下打印二叉树.py
@@ -6,4 +6,43 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
-    def PrintFromToBottom(self, root):
+
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        # write code here
+        if root is None:
+            return []
+
+        queue = []
+        output = []
+
+        queue.append(root)
+
+        while len(queue) > 0:
+            currentRoot = queue.pop(0)
+            output.append(currentRoot.val)
+            if currentRoot.left:
+                queue.append(currentRoot.left)
+            if currentRoot.right:
+                queue.append(currentRoot.right)
+        return output
+
+
+pNode1 = TreeNode(8)
+pNode2 = TreeNode(6)
+pNode3 = TreeNode(10)
+pNode4 = TreeNode(5)
+pNode5 = TreeNode(7)
+pNode6 = TreeNode(9)
+pNode7 = TreeNode(11)
+
+pNode1.left = pNode2
+pNode1.right = pNode3
+pNode2.left = pNode4
+pNode2.right = pNode5
+pNode3.left = pNode6
+pNode3.right = pNode7
+
+S = Solution()
+print(S.PrintFromTopToBottom(pNode1))
