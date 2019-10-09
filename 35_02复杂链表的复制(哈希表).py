d1ad7f4cfb72015820cfca58c49f203bdaf18c7c
jiyali <762115542@qq.com>
2019-10-09 20:36:08 +0800

    添加新题解

--- 35_02复杂链表的复制(哈希表).py
+++ 35_02复杂链表的复制(哈希表).py
@@ -20,14 +20,16 @@ class Solution(object):
             labelList.append(pNode.label)
             pNode = pNode.next
 
-        # 设置random节点的索引
+        # 设置random节点的索引，没有则设置为-1
         labelIndexList = list(map(lambda x: nodeList.index(x) if x else -1, randomList))
 
         pClone = RandomListNode(0)
         pCloneNode = pClone
 
+        # 设置节点列表
         nodeList = list(map(lambda x: RandomListNode(x), labelList))
 
+        # 绑定每个节点的random
         for i in range(len(nodeList)):
             if labelIndexList[i] != -1:
                 nodeList[i].random = nodeList[labelIndexList[i]]
