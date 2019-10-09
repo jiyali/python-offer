00783efa2b108a2ae1c6ecba3f439affd61444a2
jiyali <762115542@qq.com>
2019-10-09 09:31:02 +0800

    添加新题解

---
+++ 32_02分行从上到下打印二叉树.py
@@ -0,0 +1,53 @@
+# 题目：从上到下按层打印二叉树，同一层的节点按照从左到右的顺序打印，每一层打印到一行
+
+
+class TreeNode(object):
+    def __init__(self, data):
+        self.val = data
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    # 返回二维列表[[1,2],[4,5]]
+    def BranchesPrintFromTopToBottom(self, pRoot):
+        # write code here
+        if pRoot is None:
+            return []
+
+        nodes = [pRoot]
+        output = []
+
+        while nodes:
+            curStack = []
+            nextStack = []
+
+            for node in nodes:
+                curStack.append(node.val)
+                if node.left:
+                    nextStack.append(node.left)
+                if node.right:
+                    nextStack.append(node.right)
+            output.append(curStack)
+            nodes = nextStack
+        return output
+
+
+pNode1 = TreeNode(8)
+pNode2 = TreeNode(6)
+pNode3 = TreeNode(10)
+pNode4 = TreeNode(5)
+pNode5 = TreeNode(7)
+pNode6 = TreeNode(9)
+pNode7 = TreeNode(11)
+
+pNode1.left = pNode2
+pNode1.right = pNode3
+pNode2.left = pNode4
+pNode2.right = pNode5
+pNode3.left = pNode6
+pNode3.right = pNode7
+
+S = Solution()
+aList = S.BranchesPrintFromTopToBottom(pNode1)
+print(aList)
