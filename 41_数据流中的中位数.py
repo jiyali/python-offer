b6869594f2ffec13390ad50fc15c6fbc4b3c21a2
jiyali <762115542@qq.com>
2019-10-14 22:00:52 +0800

    添加新题解

--- 41_数据流中的中位数.py
+++ 41_数据流中的中位数.py
@@ -24,7 +24,8 @@ class Solution(object):
             return self.data[int(length // 2)]
 
 
-# 思路二：将数据流中的数据存入数组中，按照Partition的思想排序
+# 思路二：将数据流中的数据存入数组中，按照Partition的思想排序，插入的时间复杂度为O(1)，输出时间复杂度为O(n)，
+#         这里如果使用快排的话，时间复杂度是O(nlogn)
 class Solution1(object):
     def __init__(self):
         self.arr = []
@@ -32,7 +33,7 @@ class Solution1(object):
     def Insert(self, num):
         self.arr.append(num)
 
-    def GetMedian(self):  # 牛客上需要加一个变量 def GetMedian(self,arr)这样，我也不知道为啥
+    def GetMedian(self):  # 牛客上需要加一个变量 def GetMedian(self,n = None)这样，我也不知道为啥
         length = len(self.arr)
         if length == 0:
             return []
@@ -67,8 +68,103 @@ class Solution1(object):
 
         return left
 
+# 思路三：最炫酷的就是堆
+import heapq
 
-s = Solution1()
+
+class Solution2(object):
+
+    def __init__(self):
+        self.minNums = []
+        self.maxNums = []
+
+    def maxHeapInsert(self, num):
+        self.maxNums.append(num)
+        lens = len(self.maxNums)
+        i = lens - 1
+        while i > 0:
+            if self.maxNums[int(i)] > self.maxNums[int((i - 1) / 2)]:
+                t = self.maxNums[int((i - 1) / 2)]
+                self.maxNums[int((i - 1) / 2)] = self.maxNums[int(i)]
+                self.maxNums[i] = t
+                i = (i - 1) / 2
+            else:
+                break
+
+    def maxHeapPop(self):
+        t = self.maxNums[0]
+        self.maxNums[0] = self.maxNums[-1]
+        self.maxNums.pop()
+        lens = len(self.maxNums)
+        i = 0
+        while 2 * i + 1 < lens:
+            nexti = 2 * i + 1
+            if (nexti + 1 < lens) and self.maxNums[nexti + 1] > self.maxNums[nexti]:
+                nexti += 1
+            if self.maxNums[nexti] > self.maxNums[i]:
+                tmp = self.maxNums[i]
+                self.maxNums[i] = self.maxNums[nexti]
+                self.maxNums[nexti] = tmp
+                i = nexti
+            else:
+                break
+        return t
+
+    def minHeapInsert(self, num):
+        self.minNums.append(num)
+        lens = len(self.minNums)
+        i = lens - 1
+        while i > 0:
+            if self.minNums[int(i)] < self.minNums[int((i - 1) / 2)]:
+                t = self.minNums[int((i - 1) / 2)]
+                self.minNums[int((i - 1) / 2)] = self.minNums[int(i)]
+                self.minNums[i] = t
+                i = (i - 1) / 2
+            else:
+                break
+
+    def minHeapPop(self):
+        t = self.minNums[0]
+        self.minNums[0] = self.minNums[-1]
+        self.minNums.pop()
+        lens = len(self.minNums)
+        i = 0
+        while 2 * i + 1 < lens:
+            nexti = 2 * i + 1
+            if (nexti + 1 < lens) and self.minNums[nexti + 1] < self.minNums[nexti]:
+                nexti += 1
+            if self.minNums[nexti] < self.minNums[i]:
+                tmp = self.minNums[i]
+                self.minNums[i] = self.minNums[nexti]
+                self.minNums[nexti] = tmp
+                i = nexti
+            else:
+                break
+        return t
+
+    def Insert(self, num):
+        if (len(self.minNums) + len(self.maxNums)) & 1 == 0:
+            if len(self.maxNums) > 0 and num < self.maxNums[0]:
+                self.maxHeapInsert(num)
+                num = self.maxHeapPop()
+            self.minHeapInsert(num)
+        else:
+            if len(self.minNums) > 0 and num > self.minNums[0]:
+                self.minHeapInsert(num)
+                num = self.minHeapPop()
+            self.maxHeapInsert(num)
+
+    def GetMedian(self):  # 牛客上需要加一个变量 def GetMedian(self,n = None)这样，我也不知道为啥
+        allLen = len(self.minNums) + len(self.maxNums)
+        if allLen == 0:
+            return -1
+        if allLen & 1 == 1:
+            return self.minNums[0]
+        else:
+            return (self.maxNums[0] + self.minNums[0] + 0.0) / 2
+
+
+s = Solution2()
 s.Insert(5)
 print(s.GetMedian())
 s.Insert(2)
