5788735cf10cb4d483faeb12ae84c4c6b98782b2
jiyali <762115542@qq.com>
2019-10-20 19:40:52 +0800

    添加新题解

--- 48_最长不含重复字符的子字符串.py
+++ 48_最长不含重复字符的子字符串.py
@@ -2,5 +2,50 @@
 #       假设字符串中只包含从’a’到’z’的字符。
 
 
+# 思路：滑动窗口，时间复杂度O(n)
 class Solution(object):
     def longestSubstringWithoutDuplication(self, s):
+        if not s or len(s) == 0:
+            return 0
+
+        left = 0
+        lookup = set()
+
+        max_len = 0
+        cur_len = 0
+
+        for i in range(len(s)):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+            if cur_len > max_len:
+                max_len = cur_len
+            lookup.add(s[i])
+        return max_len
+
+
+# 思路二：利用哈希表
+class Solution1(object):
+    def longestSubstringWithoutDuplication(self, s):
+
+        dict = {}
+        start = 0
+        cur_len = 0
+        max_len = 0
+
+        for i in range(len(s)):
+            if s[i] in dict and dict[s[i]] >= start:
+                start = dict[s[i]]+1
+            dict[s[i]] = i
+            cur_len = i - start + 1
+            if cur_len > max_len:
+                max_len = cur_len
+        return max_len
+
+
+s = Solution1()
+print(s.longestSubstringWithoutDuplication('arabcacfr'))
+
+
