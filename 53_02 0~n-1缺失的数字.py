adb3d49b25809d018f20cddbeadda867178d4f7b
jiyali <762115542@qq.com>
2019-10-23 18:25:27 +0800

    添加新题解

--- 53_02 0~n-1缺失的数字.py
+++ 53_02 0~n-1缺失的数字.py
@@ -2,6 +2,7 @@
 #       在范围 0 到 n-1 的 n 个数字中有且只有一个数字不在该数组中，请找出这个数字。
 
 
+# 常规二分法
 class Solution(object):
     def getMissingNumber(self, nums):
         if nums is None or len(nums) == 0:
@@ -19,5 +20,30 @@ class Solution(object):
         return left
 
 
-s = Solution()
-print(s.getMissingNumber([0, 1, 2, 4]))
+# 改进二分法
+class Solution1(object):
+    def getMissingNumber(self, nums):
+        if nums is None or len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return 1
+
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            mid = left + (right - left + 1) // 2
+            if nums[mid] != mid:
+                if nums[mid - 1] == mid - 1:
+                    return mid
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
+
+
+s = Solution1()
+print(s.getMissingNumber([0]))
+print(s.getMissingNumber([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26,
+                          27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]))
