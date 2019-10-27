741d6d35d3047df4544290bc34970dd4bfab988f
jiyali <762115542@qq.com>
2019-10-27 21:36:17 +0800

    添加新题解

--- 59_滑动窗口的最大值.py
+++ 59_滑动窗口的最大值.py
@@ -18,16 +18,44 @@ class Solution(object):
         res = []
         window = []
 
-        for i, val in enumerate(num):
-            while len(window) >= 1 and i >= size and window[0] <= i - size:
+        for i in range(len(num)):
+
+            # 考虑什么时候，要把最大移除
+            # 左边界划出的时候，应该是 window.pop(0)
+            # [0,1,2,3,4]
+            #     [    i]
+            # window[0] == i - k 这个条件特别容易忽略
+            if i >= size and window[0] == i - size:
                 window.pop(0)
-            while len(window) >= 1 and val >= num[window[-1]]:
-                window.pop(-1)
+            # 考虑把不可能是最大的元素全部 kill 掉
+            while window and num[i] >= num[window[-1]]:
+                window.pop()
+            # 不管怎么着都加当前的索引
             window.append(i)
+
+            # 什么时候有滑动窗口呢？
             if i >= size - 1:
                 res.append(num[window[0]])
         return res
 
 
-s = Solution()
+class Solution1:
+    def maxInWindows(self, num, size):
+        """算法一样，但是更简洁"""
+        if len(num) == 0 or size <= 0 or len(num) < size:
+            return []
+        result = []
+        window_max = max(num[0:size])
+        result.append(window_max)
+        for i in range(size, len(num)):
+            if num[i] > window_max:
+                window_max = num[i]
+            elif num[i - size] == window_max:  # 这个是要出来的
+                window_max = max(num[i - size + 1:i + 1])
+            result.append(window_max)
+
+        return result
+
+
+s = Solution1()
 print(s.maxInWindows([2, 3, 4, 2, 6, 2, 5, 1], 3))
