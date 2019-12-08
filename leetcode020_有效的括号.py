7855535751af0cc1f74bd5498a6ca12eab518e49
jiyali <762115542@qq.com>
2019-12-08 16:04:01 +0800

    添加新题解

---
+++ leetcode020_有效的括号.py
@@ -0,0 +1,23 @@
+# 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+#
+# 有效字符串需满足：
+#
+# 左括号必须用相同类型的右括号闭合。
+# 左括号必须以正确的顺序闭合。
+# 注意空字符串可被认为是有效字符串。
+
+
+class Solution(object):
+    def isValid(self, s):
+        dic = {'{': '}', '[': ']', '(': ')', '': ''}
+        stack = ['']
+        for c in s:
+            if c in dic:
+                stack.append(c)
+            elif dic[stack.pop()] != c:
+                return False
+        return len(stack) == 1
+
+
+s = Solution()
+print(s.isValid("]"))
