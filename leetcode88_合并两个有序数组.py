f53ee22605cc01ac0a875fc5e9632c5f2e6b9054
jiyali <762115542@qq.com>
2020-01-08 17:13:45 +0800

    添加新题解

---
+++ leetcode88_合并两个有序数组.py
@@ -0,0 +1,30 @@
+# 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        nums1[:p2 + 1] = nums2[:p2 + 1]
+        return nums1
+
+
+s = Solution()
+print(s.merge(nums1=[1, 2, 3, 0, 0, 0], m=3, nums2=[2, 5, 6], n=3))
