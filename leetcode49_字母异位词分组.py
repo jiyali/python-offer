ec84aab9e3ff4874426e856a9df75d95114b83d8
jiyali <762115542@qq.com>
2020-01-12 13:43:55 +0800

    添加新题解

--- leetcode49_字母异位词分组.py
+++ leetcode49_字母异位词分组.py
@@ -17,6 +17,19 @@ class Solution:
                 dic[key].append(i)
         return list(dic.values())
 
+    def groupAnagrams1(self, strs):
+        from collections import defaultdict
+        # defaultdict属于内建函数dict的一个子类，调用工厂函数提供缺失的值
+        prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
+        lookup = defaultdict(list)
+        for _str in strs:
+            key_val = 1
+            for s in _str:
+                key_val *= prime[ord(s) - 97]
+            lookup[key_val].append(_str)
+        return list(lookup.values())
+
 
 s = Solution()
 print(s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+print(s.groupAnagrams1(["eat", "tea", "tan", "ate", "nat", "bat"]))
