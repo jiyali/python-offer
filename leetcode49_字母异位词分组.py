6aa4d11f8342b34f3b6ca6fbca29c7f6d87bb88b
jiyali <762115542@qq.com>
2020-01-12 13:53:27 +0800

    添加新题解

--- leetcode49_字母异位词分组.py
+++ leetcode49_字母异位词分组.py
@@ -22,11 +22,11 @@ class Solution:
         # defaultdict属于内建函数dict的一个子类，调用工厂函数提供缺失的值
         prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
         lookup = defaultdict(list)
-        for _str in strs:
+        for string in strs:
             key_val = 1
-            for s in _str:
-                key_val *= prime[ord(s) - ord('a')]
-            lookup[key_val].append(_str)
+            for char in string:
+                key_val *= prime[ord(char) - ord('a')]
+            lookup[key_val].append(string)
         return list(lookup.values())
 
 
