b3927a6ce1e3ee5652df23024b35e8d229d49b28
jiyali <762115542@qq.com>
2020-01-15 20:01:25 +0800

    添加新题解

--- leetcode023_合并k个排序链表.py
+++ leetcode023_合并k个排序链表.py
@@ -11,25 +11,30 @@ class Solution:
     def mergeKLists(self, lists):
         if not lists:
             return None
-        n = len(lists)
-        return self.merge(lists, 0, n - 1)
+
+        length = len(lists)
+
+        return self.merge(lists, 0, length - 1)
 
     def merge(self, lists, left, right):
         if left == right:
             return lists[left]
-        mid = left + (right - left) // 2
-        l1 = self.merge(lists, left, mid)
-        l2 = self.merge(lists, mid + 1, right)
-        return self.mergeTwoLists(l1, l2)
 
-    def mergeTwoLists(self, l1, l2):
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        if l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
+        mid = left + (right - left) // 2
+
+        list1 = self.merge(lists, left, mid)
+        list2 = self.merge(lists, mid + 1, right)
+        return self.mergeTwoLists(list1, list2)
+
+    def mergeTwoLists(self, list1, list2):
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
         else:
-            l2.next = self.mergeTwoLists(l1, l2.next)
-            return l2
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
