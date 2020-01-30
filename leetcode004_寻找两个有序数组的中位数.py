3dbc267352f4fa2dac7233c9effdebe7bba40905
jiyali <762115542@qq.com>
2020-01-30 21:23:00 +0800

    添加新题解

--- leetcode004_寻找两个有序数组的中位数.py
+++ leetcode004_寻找两个有序数组的中位数.py
@@ -1,6 +1,7 @@
 # 题目： 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
 #        请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
 #        你可以假设 nums1 和 nums2 不会同时为空
+# 解题分析：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/dong-yong-er-fen-cha-zhao-mo-ban-lai-qiao-miao-jie/
 
 
 class Solution(object):
@@ -13,14 +14,14 @@ class Solution(object):
 
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
-        len1, len2 = len(nums1), len(nums2)
+        m, n = len(nums1), len(nums2)
 
-        left, right, half_len = 0, len1, (len1 + len2 + 1) // 2
+        left, right, half_len = 0, m, (m + n + 1) // 2
         mid1 = (left + right) // 2
         mid2 = half_len - mid1
 
         while left < right:
-            if mid1 < len1 and nums2[mid2 - 1] > nums1[mid1]:
+            if mid1 < m and nums2[mid2 - 1] > nums1[mid1]:
                 left = mid1 + 1
             else:
                 right = mid1
@@ -34,12 +35,12 @@ class Solution(object):
         else:
             max_of_left = max(nums1[mid1 - 1], nums2[mid2 - 1])
 
-        if (len1 + len2) % 2 == 1:
+        if (m + n) % 2 == 1:
             return max_of_left
 
-        if mid1 == len1:
+        if mid1 == m:
             min_of_right = nums2[mid2]
-        elif mid2 == len2:
+        elif mid2 == n:
             min_of_right = nums1[mid1]
         else:
             min_of_right = min(nums1[mid1], nums2[mid2])
