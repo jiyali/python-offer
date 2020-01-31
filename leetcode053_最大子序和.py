fe634080f531739f5d3354ce1dbc17180a6a3a5c
jiyali <762115542@qq.com>
2020-01-31 23:51:30 +0800

    添加新题解

---
+++ leetcode053_最大子序和.py
@@ -0,0 +1,15 @@
+# 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+
+class Solution:
+    def maxSubArray(self, nums):
+        # 普通做法
+        cur_sum = max_sum = nums[0]
+        for i in range(1, len(nums)):
+            cur_sum = max(nums[i], cur_sum + nums[i])
+            max_sum = max(cur_sum, max_sum)
+        return max_sum
+
+
+s = Solution()
+print(s.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
