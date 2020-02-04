99f94280a5f584abd8deca7062de55f2398189f1
jiyali <762115542@qq.com>
2020-02-04 12:29:04 +0800

    添加新题解

---
+++ 冒泡排序.py
@@ -0,0 +1,14 @@
+class Solution:
+    def bubbleSort(self, arr):
+        if arr is None or len(arr) <2:
+            return arr
+
+        for i in range(len(arr)):
+            for j in range(0, len(arr) - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        return arr
+
+
+s = Solution()
+print(s.bubbleSort([64, 34, 25, 12, 22, 11, 90]))
