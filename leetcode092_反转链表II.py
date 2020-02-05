3fd6072f8a2c81008f251cdd8f74ccfdb8e6585c
jiyali <762115542@qq.com>
2020-02-05 17:18:29 +0800

    添加新题解

--- leetcode092_反转链表II.py
+++ leetcode092_反转链表II.py
@@ -7,20 +7,18 @@ class ListNode():
         self.val = val
 
 
-class Solution():
+class Solution:
     def reverseBetween(self, head, m, n):
-        dummy = ListNode(-1)
-        dummy.next = head
-        pre = dummy  # 反转点前一个
+        pre = pre_head = ListNode(0)
+        pre.next = head
 
         for i in range(m - 1):
             head = head.next
             pre = pre.next
-        # pre是反转点前一个, head是反转点
-        for i in range(n - m):
-            nxt = head.next  # 记录反转点后一个
-            head.next = nxt.next  # 反转nxt和head
-            nxt.next = pre.next  # 重新链接
-            pre.next = nxt
 
-        return dummy.next
+        for j in range(n - m):
+            tmp = head.next
+            head.next = tmp.next
+            tmp.next = pre.next
+            pre.next = tmp
+        return pre_head.next
