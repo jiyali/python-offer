03a2b1ae8b5ab5f4e6a7f139e7054a8b5bd7f174
jiyali <762115542@qq.com>
2020-02-06 00:00:55 +0800

    添加新题解

---
+++ leetcode143_重排链表.py
@@ -0,0 +1,46 @@
+# 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+# 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+#
+# 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+
+class ListNode:
+    def __init__(self, val):
+        self.next = None
+        self.val = val
+
+
+class Solution:
+    def reorderList(self, head):
+        if not head or not head.next: return head
+        # 1 2 3 4 5
+        fast = head
+        pre_mid = head
+        # 找到中点, 偶数个找到时上界那个
+        while fast.next and fast.next.next:
+            pre_mid = pre_mid.next
+            fast = fast.next.next
+        # 翻转中点之后的链表,采用是pre, cur双指针方法
+        pre = None
+        cur = pre_mid.next
+        # 1 2 5 4 3
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tmp
+        # 翻转链表和前面链表拼接
+        pre_mid.next = pre
+        # 1 5 2 4 3
+        # 链表头
+        p1 = head
+        # 翻转头
+        p2 = pre_mid.next
+        # print(p1.val, p2.val)
+        while p1 != pre_mid:
+            # 建议大家这部分画图, 很容易理解
+            pre_mid.next = p2.next
+            p2.next = p1.next
+            p1.next = p2
+            p1 = p2.next
+            p2 = pre_mid.next
