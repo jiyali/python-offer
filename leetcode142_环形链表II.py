e1638e72ae1a4016415d76c40df64b8785eac718
jiyali <762115542@qq.com>
2020-02-06 18:17:33 +0800

    添加新题解

---
+++ leetcode142_环形链表II.py
@@ -0,0 +1,31 @@
+# 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+#
+# 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+#
+# 说明：不允许修改给定的链表
+
+
+class ListNode:
+    def __init__(self, val):
+        self.next = None
+        self.val = val
+
+
+class Solution:
+    def detectCycle(self, head):
+        if not head or not head.next:
+            return None
+
+        slow = fast = head
+        while True:
+            if not fast or not fast.next:
+                return None
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                break
+        fast = head
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+        return fast
