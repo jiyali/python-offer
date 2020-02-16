b251452e7002a23cd8dcbcf7ac2f09f293d456fa
jiyali <762115542@qq.com>
2020-02-16 23:56:26 +0800

    添加新题解

---
+++ leetcode103_二叉树的锯齿形层次遍历.py
@@ -0,0 +1,43 @@
+# 给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+#
+# 例如：
+# 给定二叉树 [3,9,20,null,null,15,7],
+#
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# 返回锯齿形层次遍历如下：
+#
+# [
+#   [3],
+#   [20,9],
+#   [15,7]
+# ]
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def zigzagLevelOrder(self, root):
+        res = []
+
+        def helper(root, depth):
+            if not root:
+                return None
+            if len(res) == depth:
+                res.append([])
+            if depth % 2 == 0:
+                res[depth].append(root.val)
+            else:
+                res[depth].insert(root.val)
+            helper(root.left, depth + 1)
+            helper(root.right, depth + 1)
+        helper(root, 0)
+        return res
