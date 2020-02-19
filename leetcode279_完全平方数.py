6ae16ffd470f6c9601b87f93d08bdb9620e80061
jiyali <762115542@qq.com>
2020-02-19 09:21:04 +0800

    添加新题解

--- leetcode279_完全平方数.py
+++ leetcode279_完全平方数.py
@@ -5,12 +5,30 @@
 
 class Solution:
     def numSquares(self, n):
+        # 动态规划
         dp = [i for i in range(n + 1)]
-        for i in range(2, n + 1):
-            for j in range(1, int(i ** (0.5)) + 1):
-                dp[i] = min(dp[i], dp[i - j * j] + 1)
+        m = 1
+        while m * m <= n:
+            for i in range(m * m, n + 1):
+                dp[i] = min(dp[i], dp[i - m * m] + 1)
+            m += 1
         return dp[-1]
+
+    def numSquares_bfs(self, n):
+        # bfs 所有可能的平方数是从1到floor(sqrt(n))，找到最小的层数刚好满足和为n
+        # Time O(n**n/2) SpaceO(n)
+        queue = [(n, 0)]
+        while queue:
+            res, depth = queue.pop(0)
+            k = int(res ** 0.5)
+            for i in range(k, 0, -1):
+                new_res = res - i ** 2
+                if new_res == 0:
+                    return depth + 1
+                queue.append((new_res, depth + 1))
+        return res
 
 
 s = Solution()
 print(s.numSquares(12))
+print(s.numSquares_bfs(12))
