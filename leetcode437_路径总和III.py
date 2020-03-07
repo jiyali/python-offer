dc8cf2c39587131ebbe74da4a1639ce0afc11769
jiyali <762115542@qq.com>
2020-03-07 23:33:38 +0800

    添加新题解

---
+++ leetcode437_路径总和III.py
@@ -0,0 +1,47 @@
+# 给定一个二叉树，它的每个结点都存放着一个整数值。
+#
+# 找出路径和等于给定数值的路径总数。
+#
+# 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+#
+# 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+#
+# 示例：
+#
+# root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+#
+#       10
+#      /  \
+#     5   -3
+#    / \    \
+#   3   2   11
+#  / \   \
+# 3  -2   1
+#
+# 返回 3。和等于 8 的路径有:
+#
+# 1.  5 -> 3
+# 2.  5 -> 2 -> 1
+# 3.  -3 -> 11
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def pathSum(self, root, sum):
+        if not root:
+            return 0
+
+        def helper(root, sum_):
+            if not root:
+                return 0
+            sum_ -= root.val
+            cur = 1 if sum_ == 0 else 0
+            return helper(root.left, sum_) + helper(root.right, sum_) + cur
+
+        return helper(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
