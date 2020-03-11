f30fbfa3c5a527154003f73ae5f7dcdc025fb555
jiyali <762115542@qq.com>
2020-03-11 17:43:35 +0800

    添加新题解

---
+++ leetcode022_括号生成.py
@@ -0,0 +1,32 @@
+# 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+#
+# 例如，给出 n = 3，生成结果为：
+#
+# [
+#   "((()))",
+#   "(()())",
+#   "(())()",
+#   "()(())",
+#   "()()()"
+# ]
+
+
+class Solution:
+    def generateParenthesis(self, n):
+        if n == 0:
+            return [""]
+        elif n == 1:
+            return ["()"]
+        elif n == 2:
+            return ["()()", "(())"]
+        result = []
+        for i in range(n):
+            j = n - 1 - i
+            temp1 = self.generateParenthesis(i)
+            temp2 = self.generateParenthesis(j)
+            result.extend(["(%s)%s" % (p, q) for p in temp1 for q in temp2])
+        return result
+
+
+s = Solution()
+print(s.generateParenthesis(3))
