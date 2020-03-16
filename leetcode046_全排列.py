7e40ca391025ca1d33544e3ca565a15c85cd7ee1
jiyali <762115542@qq.com>
2020-03-16 14:57:04 +0800

    添加新题解

--- leetcode046_全排列.py
+++ leetcode046_全排列.py
@@ -15,13 +15,15 @@
 
 
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute(self, nums):
         res = []
+
         def backtrack(nums, tmp):
             if not nums:
                 res.append(tmp)
                 return
             for i in range(len(nums)):
-                backtrack(nums[: i] + nums[i+1:], tmp + [nums[i]])
+                backtrack(nums[: i] + nums[i + 1:], tmp + [nums[i]])
+
         backtrack(nums, [])
         return res
