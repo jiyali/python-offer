0f42a575b06fc167db1cd4035f85de44cc4d6511
jiyali <762115542@qq.com>
2020-03-19 20:02:03 +0800

    添加新题解

---
+++ leetcode090_子集II.py
@@ -0,0 +1,38 @@
+# 给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+#
+# 说明：解集不能包含重复的子集。
+#
+# 示例:
+#
+# 输入: [1,2,2]
+# 输出:
+# [
+#   [2],
+#   [1],
+#   [1,2,2],
+#   [2,2],
+#   [1,2],
+#   []
+# ]
+
+
+class Solution:
+    def subsetsWithDup(self, nums):
+        if not nums:
+            return []
+
+        nums.sort()
+        res = []
+
+        def backtrack(i, tmp):
+            if tmp not in res:
+                res.append(tmp)
+            for j in range(i, len(nums)):
+                backtrack(j + 1, tmp + [nums[j]])
+
+        backtrack(0, [])
+        return res
+
+
+s = Solution()
+print(s.subsetsWithDup([1, 2, 2]))
