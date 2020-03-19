4e578abc506b1db8f598426c23ebfce03dbbfc9f
jiyali <762115542@qq.com>
2020-03-19 11:46:08 +0800

    添加新题解

---
+++ leetcode049_组合总和II.py
@@ -0,0 +1,43 @@
+# 给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+#
+# candidates 中的每个数字在每个组合中只能使用一次。
+#
+# 说明：
+#
+# 所有数字（包括目标数）都是正整数。
+# 解集不能包含重复的组合。
+# 示例 1:
+#
+# 输入: candidates = [10,1,2,7,6,1,5], target = 8,
+# 所求解集为:
+# [
+#   [1, 7],
+#   [1, 2, 5],
+#   [2, 6],
+#   [1, 1, 6]
+# ]
+
+
+class Solution:
+    def combinationSum2(self, candidates, target):
+        res = []
+        candidates.sort()
+
+        def backtrack(i, tmp_sum, tmp):
+            if tmp_sum == target:
+                res.append(tmp)
+                return
+            elif tmp_sum > target or i == len(candidates):
+                return
+            else:
+                for j in range(i, len(candidates)):
+                    if j > i and candidates[j] == candidates[j - 1]:
+                        continue
+                    backtrack(j + 1, tmp_sum + candidates[j], tmp + [candidates[j]])
+
+        backtrack(0, 0, [])
+        return res
+
+
+s = Solution()
+print(s.combinationSum2(candidates=[10, 1, 2, 7, 6, 1, 5], target=8))
