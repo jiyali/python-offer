d0930154996a0a44f3dbcf87eed6c884864c82bf
jiyali <762115542@qq.com>
2020-04-01 16:42:08 +0800

    添加新题解

--- leetcode1143_最长公共子序列.py
+++ leetcode1143_最长公共子序列.py
@@ -14,24 +14,41 @@
 # 解释：最长公共子序列是 "ace"，它的长度为 3。
 
 
+class Solution1:
+    def longestCommonSubsequence(self, text1, text2):
+        if not text1 or not text2:
+            return 0
+
+        def dp(i, j):
+            if i == -1 or j == -1:
+                return 0
+            if text1[i] == text2[i]:
+                return dp(i - 1, j - 1) + 1
+            else:
+                return max(dp(i - 1, j), dp(i, j - 1))
+
+        return dp(len(text1) - 1, len(text2)-1)
+
+
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    def longestCommonSubsequence(self, text1, text2):
         m = len(text1)
         n = len(text2)
 
         dp = [[0] * (n + 1) for _ in range(m + 1)]
-        max_len = 0
-
+        # 进行状态转移
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if text1[i - 1] == text2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    # 找到一个 lcs 中的字符
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
                 else:
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-                if max_len <= dp[i][j]:
-                    max_len = dp[i][j]
-        return max_len
+
+        return dp[-1][-1]
 
 
-s = Solution()
-print(s.longestCommonSubsequence(text1="abc", text2="abc"))
+s1 = Solution()
+s2 = Solution1()
+print(s1.longestCommonSubsequence(text1="abc", text2="abc"))
+print(s2.longestCommonSubsequence(text1="abc", text2="abc"))
