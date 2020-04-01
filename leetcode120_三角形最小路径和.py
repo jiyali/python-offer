041752365663f1e1769fd476fd05dc4e990623f8
jiyali <762115542@qq.com>
2020-04-01 11:21:44 +0800

    添加新题解

--- leetcode120_三角形最小路径和.py
+++ leetcode120_三角形最小路径和.py
@@ -9,6 +9,13 @@
 #   [4,1,8,3]
 # ]
 # 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+# 从第二行开始，遍历区间[1,n)[1,n)：
+# 对每一层的元素进行遍历，遍历区间[0,len(triangle[i]))[0,len(triangle[i]))，存在三种情况：
+#    每一行的首位，triangle[i][j]+=triangle[i-1][j]triangle[i][j]+=triangle[i−1][j]，等于上一行的相同索引处。
+#    每一行的末位，triangle[i][j]+=triangle[i-1][j-1]triangle[i][j]+=triangle[i−1][j−1]，等于上一行的前一位处。
+#    对于其他元素，triangle[i][j]+=min(triangle[i-1][j-1],triangle[i-1][j])triangle[i][j]+=min(triangle[i−1][j−1],triangle[i−1][j])，等于上一行中相邻的较小值加上自身。
+# 返回最后一行中的最小路径和
+
 
 
 class Solution:
