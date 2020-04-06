7fe0f3479fd2971ebb8848fb22ed4ce8114517c8
jiyali <762115542@qq.com>
2020-04-06 09:12:53 +0800

    添加新题解

--- 字典按值排序.py
+++ 字典按值排序.py
@@ -1,7 +1,8 @@
 class Solution:
     def dicSort(self, dic):
-        dic = sorted(dic.items(), key=lambda x: x[1])
-        return dic
+        dic1 = sorted(dic.items(), key=lambda x: x[1], reverse=True)  # 从大到小
+        dic2 = sorted(dic.items(), key=lambda x: x[1], reverse=False)  # 从小到大
+        return dic1, dic2
 
 # 第一个参数是需要排序的列表，第二个参数是指定key（列表中每一项的第几个元素）来进行排序。
 # d.items()返回的是一个列表 [('a', 56), ('b', 23), ('c', 90), ('d', 12), ('e', 64), ('f', 41), ('g', 88)]
